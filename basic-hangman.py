dafaf24597532dc9b2d9be13a5a40523ee10fde8
eladWinner <123249403+eladWinner@users.noreply.github.com>
2023-02-12 17:20:04 +0200

    Update basic-hangman.py
    changed to "clean code" method ?

--- basic-hangman.py
+++ basic-hangman.py
@@ -1,123 +1,16 @@
 # This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+MAX_tries =6
+class hang_man():
+    def __init__(self):
+        self.MAX_TRIES = MAX_tries
+        self.guessed_letters_histogram = [False for i in range(ord('z')-ord('a')+1)] # The English Alphabet (or Modern English Alphabet) today consists of 26 letters
+        self.secret_word = ""
+        self.current_guess_num = 0
+        self.HANGMAN_ASCII_ART,self.HANGMAN_PHOTOS = self.load_art()
 
-
-def print_hi(name: object) -> object:
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-def main():
-
-    def choose_word(file_path, index):
-        """
-
-        :param file_path: self explanatory
-        :param index: word spot in file ( with looping )
-        :return:  the selected word
-        """
-        # dictionary={} # not used now
-        words = []
-        with open(file_path, 'r') as file:
-            lines = file.readlines()
-            for line in lines :
-                line=line.strip('\n')
-                for word in line.split(" "):
-                    #dictionary[word]=False
-                    words.append(word)
-        # number_of_unique_words = len(dictionary)
-        index = (int(index) -1)%len(words) # instead of repeating
-        return words[index]
-
-    def check_valid_input(letter_guessed, old_letters_guessed):
-        """
-
-        :param letter_guessed: the guess
-        :param old_letters_guessed: history of old guesses
-        :return: boolean if valid
-        """
-        if len(letter_guessed)!=1 or not isinstance(letter_guessed, str):
-            return False
-        char_value = ord(letter_guessed)
-        if char_value >= ord('a'): # A= 65 , a=97
-            char_value -= ord('a')
-        else:
-            char_value -= ord('A')
-        if char_value >=(ord('z')-ord('a')+1) or char_value <0 :
-            return False
-        return not old_letters_guessed[char_value] # returns the opposite of history if played did guess latter
-        #tl:dr ' if player did **NOT** guess that letter before.'
-
-    def try_update_letter_guessed(letter_guessed, old_letters_guessed):
-        """
-
-        :param letter_guessed: above
-        :param old_letters_guessed:
-        :return: boolean if manged to replace letter
-        """
-        if check_valid_input (letter_guessed, old_letters_guessed):
-            old_letters_guessed[ord(letter_guessed.lower())-ord('a')] = True
-            return True
-        # no need for else
-        print("X")
-        first = True
-        # prints in order letters guessed before
-        for i in range(ord('z')-ord('a')+1):
-            if (old_letters_guessed[i]):
-                if not first:
-                    print(' -> ',end='')
-                first= False
-                print(chr(i+ord('a')),end='')
-        if not first:
-            print()
-        return False
-
-    def show_hidden_word(secret_word, old_letters_guessed):
-        """
-
-        :param secret_word: self
-        :param old_letters_guessed: above
-        :return: string of visible parts of secret word based on history
-        """
-        string_builder =[]
-        aCHAR = ord('a')
-        first = True
-        for i in secret_word:
-            if not first:
-                string_builder.append(" ")
-            first = False
-            if old_letters_guessed[ord(i)-aCHAR]:
-                string_builder.append(i)
-            else:
-                string_builder.append("_")
-        return ''.join(string_builder)
-
-    def check_win(secret_word, old_letters_guessed):
-        """
-
-        :param secret_word:
-        :param old_letters_guessed:
-        :return: boolean if won
-        """
-        for i in secret_word:
-            if not old_letters_guessed[ord(i)-ord('a')]:
-                return False # leave function
-        return True
-    # aiding functions
-    #############################################
-    # start function
-
-    def Start_Game():
-        """
-        :return: void . starts game
-        """
-        MAX_TRIES = 6
-        old_letters_guessed = [False for i in range(ord('z')-ord('a')+1)] # The English Alphabet (or Modern English Alphabet) today consists of 26 letters
-        secret_word = ""
-        num_of_tries = 0
-        HANGMAN_ASCII_ART = """
+    def load_art(self):
+        return  """
       _    _                                         
      | |  | |                                        
      | |__| | __ _ _ __   __ _ _ __ ___   __ _ _ __  
@@ -125,8 +18,7 @@ def main():
      | |  | | (_| | | | | (_| | | | | | | (_| | | | |
      |_|  |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
                           __/ |                      
-                         |___/"""
-        HANGMAN_PHOTOS = [
+                         |___/""",[
             """
         x-------x
             """
@@ -180,48 +72,112 @@ def main():
         |
             """
         ]
-        # Print the Welcome Screen
-        print(HANGMAN_ASCII_ART)
-        print(MAX_TRIES)
-        print()
+    def Start_Game(self):
+        """
+        :return: void . starts game
+        """
+        def print_startscreen():
+            print(self.HANGMAN_ASCII_ART)
+            print(self.MAX_TRIES)
+            print()
 
-        # ask for file paths and index
-        file_path = input("Enter file path: ")
-        index = input("Enter index: ")
-        secret_word = choose_word(file_path, index)
-        secret_word = secret_word.lower()
+        def start_inputs(): # ask for file paths and index
+            def choose_word(file_path, index): # is reading twice faster than , 1 read+ 1 write copy?
+                words = []
+                with open(file_path, 'r') as file:
+                    lines = file.readlines()
+                    for line in lines :
+                        line=line.strip('\n')
+                        for word in line.split(" "):
+                            words.append(word)
+                index = (int(index) -1)%len(words) # instead of repeating
+                return words[index]
+            file_path = input("Enter file path: ")
+            index = input("Enter index: ")
+            self.secret_word = choose_word(file_path, index).lower()
 
-        # start game loop
-        print("Let’s start!\n")
-        print(HANGMAN_PHOTOS[num_of_tries])
-        print(show_hidden_word(secret_word, old_letters_guessed))
-        print()
+        def print_start_squance():
+            print("Let’s start!\n")
+            print(self.HANGMAN_PHOTOS[self.current_guess_num])
+            print(show_hidden_word(self.secret_word, self.guessed_letters_histogram))
+            print()
 
-        # game has two exit conditions using too many tries or winning
-        while num_of_tries < MAX_TRIES:
+        def valid_guess():
+            def check_valid_input(letter_guessed, guessed_letters_histogram):
+                if not isinstance(letter_guessed, str) or len(letter_guessed)!=1:
+                    return False
+                if not letter_guessed.isalpha():
+                    return False
+                char_value = ord(letter_guessed.lower())-ord('a')
+                return not guessed_letters_histogram[char_value] # returns the letter is not in guess history
+
+            def try_update_letter_guessed(letter_guessed, guessed_letters_histogram):
+                def prtint_previous_guess(guess_hist): # history and histogram
+                    for i in range(ord('z')-ord('a')+1):
+                        if (guess_hist[i]):
+                            if not first:
+                                print(' -> ',end='')
+                            first= False
+                            print(chr(i+ord('a')),end='')
+
+                if check_valid_input (letter_guessed, guessed_letters_histogram):
+                    guessed_letters_histogram[ord(letter_guessed.lower())-ord('a')] = True
+                    return True
+                print("X") # means in correct guess
+                first = True
+                prtint_previous_guess(guessed_letters_histogram)# prints in order letters guessed before
+                if not first:
+                    print()
+                return False
+
             guess = input("Guess a letter: ")
-            if try_update_letter_guessed(guess, old_letters_guessed):
-                if secret_word.count(guess)>0:
-                    if check_win(secret_word ,old_letters_guessed):
-                        print(show_hidden_word(secret_word, old_letters_guessed))
-                        print("WIN")
-                        # exit condition 2
-                        return
+            if try_update_letter_guessed(guess, self.guessed_letters_histogram):
+                return guess
+            return valid_guess() # while true
+
+        def check_win_condation():
+            if self.check_win(self.secret_word ,self.guessed_letters_histogram):
+                print(self.show_hidden_word(self.secret_word, self.guessed_letters_histogram))
+                print("WIN")
+                return True
+            return False
+
+        def show_hidden_word(secret_word, guessed_letters_histogram):
+            string_builder =[]
+            first = True
+            for i in secret_word:
+                if not first:
+                    string_builder.append(" ")
+                first = False
+                if guessed_letters_histogram[ord(i)-ord('a')]:
+                    string_builder.append(i)
                 else:
-                    print(":(")
-                    num_of_tries = num_of_tries + 1
-                    print(HANGMAN_PHOTOS[num_of_tries])
-                print(show_hidden_word(secret_word, old_letters_guessed))
-        # exit condition 1
+                    string_builder.append("_")
+            return ''.join(string_builder)
+
+        print_startscreen()
+        start_inputs()
+        print_start_squance()
+        # start game loop
+        while self.current_guess_num < self.MAX_TRIES:# game has two exit conditions using too many tries or winning
+            guess=valid_guess()
+            if self.secret_word.count(guess)>0:
+                if check_win_condation():
+                    return #exit game
+            else:
+                print(":(")
+                self.current_guess_num = self.current_guess_num + 1
+                print(self.HANGMAN_PHOTOS[self.current_guess_num])
+            print(show_hidden_word(self.secret_word, self.guessed_letters_histogram))
+        # exit game lose condition
         print("LOSE")
         return
-    # functions
-    #########################
-    # code
-    Start_Game()
 
+
+        
+def main():
+    hang_man_game=hang_man()
+    hang_man_game.Start_Game()
 
 if __name__ == '__main__':
     main()
-
-
